31fcd15378facf4b40128c5ce77c3df697bbe1aa
Calvince Adika <caadika@gmail.com>
2025-10-02 13:18:02 +0300

    Add performance.sql

---
+++ database-adv-script/perfromance.sql
@@ -0,0 +1,25 @@
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status AS booking_status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_method,
+    pay.payment_date
+FROM
+    bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+ORDER BY
+    b.created_at DESC;
